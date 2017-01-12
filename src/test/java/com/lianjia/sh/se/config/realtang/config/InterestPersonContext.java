4115ae52e39a69facbe552e7a57f0af012c80763
huisman <huisman@dooioo.com>
2017-01-12 18:55:44 +0800

    调整配置。。

--- src/test/java/com/lianjia/sh/se/config/realtang/config/InterestPersonContext.java
+++ src/test/java/com/lianjia/sh/se/config/realtang/config/InterestPersonContext.java
@@ -1,8 +1,23 @@
 package com.lianjia.sh.se.config.realtang.config;
 
-public class InterestPersonContext {
-  public static final String module=InterestPerson.class.getName();
-  interface Options{
-     String CITY="city";
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.lianjia.sh.se.config.regaltang.rule.context.Context;
+import com.lianjia.sh.se.config.regaltang.rule.option.CriteriaOption;
+import com.lianjia.sh.se.config.regaltang.rule.option.PredefinedValuesOption;
+import com.lianjia.sh.se.config.regaltang.rule.value.SimpleNamedValue;
+
+public class InterestPersonContext  implements Context{
+  public static final String MODULE_NAMESPACE = InterestPerson.class.getName();
+
+  interface Options {
+    CriteriaOption CITY = new PredefinedValuesOption("city","城市", 
+        Stream.of(City.values()).map((item) -> new SimpleNamedValue(item.name(), item)).collect(Collectors.toList()));
+  }
+
+  @Override
+  public Object get(String optionKey) {
+    return null;
   }
 }
--- src/test/java/com/lianjia/sh/se/config/realtang/config/ManualRuleConfigurerTest.java
+++ src/test/java/com/lianjia/sh/se/config/realtang/config/ManualRuleConfigurerTest.java
@@ -7,9 +7,7 @@ import org.junit.Test;
 
 import com.lianjia.sh.se.config.regaltang.config.ManualRuleConfigurer;
 import com.lianjia.sh.se.config.regaltang.rule.item.RuleItem;
-import com.lianjia.sh.se.config.regaltang.rule.option.PredefinedValuesOption;
 import com.lianjia.sh.se.config.regaltang.rule.output.ScalarRuleOutput;
-import com.lianjia.sh.se.config.regaltang.rule.value.SimpleNamedValue;
 
 public class ManualRuleConfigurerTest {
 
@@ -19,7 +17,7 @@ public class ManualRuleConfigurerTest {
       ManualRuleConfigurer.configurer()
       .appName("房源")
       .withModule()
-          .identity(InterestPersonContext.module)
+          .identity(InterestPersonContext.MODULE_NAMESPACE)
           .name("权益方")
           .ruleItems(
               Stream.of(InterestPerson.values())
@@ -27,10 +25,7 @@ public class ManualRuleConfigurerTest {
               .collect(Collectors.toList())
            )
           .availableOptions(
-             new PredefinedValuesOption(InterestPersonContext.Options.CITY
-               , "城市", Stream.of(City.values()).map((item) -> 
-               new SimpleNamedValue(item.name(), item))
-               .collect(Collectors.toList()))
+             InterestPersonContext.Options.CITY
            )
           .output(new ScalarRuleOutput("分佣比率",Double.class))
           .and()
